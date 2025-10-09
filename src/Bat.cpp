0d1d527e359e9cf146358137a0a63c506731fcfc
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-10-09 09:30:10 +0200

    hud, bat displayed

--- src/Bat.cpp
+++ src/Bat.cpp
@@ -1,3 +1,55 @@
 #include "Bat.h"
 
 
+Bat::Bat(float X, float Y) : m_Position(X, Y)
+{
+	m_Shape.setSize(sf::Vector2<float>(50.0f, 5.0f));
+	m_Shape.setPosition(m_Position);
+	m_Shape.setFillColor(sf::Color::Red);
+}
+
+
+sf::FloatRect Bat::getPosition()
+{
+	return m_Shape.getGlobalBounds();
+}
+
+sf::RectangleShape Bat::getShape()
+{
+	return m_Shape;
+}
+
+void Bat::moveLeft()
+{
+	m_bMovingLeft = true;
+}
+
+void Bat::moveRight()
+{
+	m_bMovingRight = true;
+}
+
+
+void Bat::stopLeft()
+{
+	m_bMovingLeft = false;
+}
+
+void Bat::stopRight()
+{
+	m_bMovingRight = false;
+}
+
+
+void Bat::update(sf::Time dt)
+{
+	if (m_bMovingRight)
+	{
+		m_Position.x += m_Speed * dt.asSeconds();
+	}
+	if (m_bMovingLeft)
+	{
+		m_Position.x -= m_Speed * dt.asSeconds();
+	}
+	m_Shape.setPosition(m_Position);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,110 @@
+#include "Bat.h"
+#include <sstream>
+#include <cstdlib>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	// Create a video mode object
+	sf::VideoMode vm({ 1920, 1080 });
+	// Create and open a window for the game
+	sf::RenderWindow window(vm, "Pong");
+	int score = 0;
+	int lives = 3;
 
-        window.clear();
-        window.display();
-    }
+	// Create a bat at the bottom center of the screen
+	Bat bat(1920 / 2, 1080 - 20);
+	// We will add a ball in the next chapter
+
+	// A cool retro-style font
+	sf::Font font;
+	font.openFromFile("fonts/ds-digital.ttf");
+	//font.loadFromFile("fonts/DS-.ttf");
+	// Create a Text object called HUD
+	sf::Text hud(font, "", 30);
+	// Set the font to our retro-style
+	hud.setFont(font);
+	// Make it nice and big
+	hud.setCharacterSize(75);
+	// Choose a color
+	hud.setFillColor(sf::Color::White);
+	hud.setPosition(sf::Vector2<float>(20, 20));
+	// Here is our clock for timing everything
+	sf::Clock clock;
+
+	while (window.isOpen())
+	{
+
+		// Process events
+		while (const auto event = window.pollEvent())
+		{
+			// Close window: exit
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+
+			// Escape pressed: exit
+			if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
+				keyPressed && keyPressed->code == sf::Keyboard::Key::Escape)
+			{
+				window.close();
+			}
+		}
+
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) 
+		{
+			bat.moveLeft();
+		}
+		else
+		{
+			bat.stopLeft();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+		{
+			bat.moveRight();
+		}
+		else
+		{
+			bat.stopRight();
+		}
+
+		sf::Time dt = clock.restart();
+		bat.update(dt);
+
+		std::stringstream ss;
+		ss << "Score:" << score << " Lives:" << lives;
+		hud.setString(ss.str());
+
+		window.clear();
+		window.draw(hud);
+		window.draw(bat.getShape());
+		window.display();
+
+		/*
+		Handle the player input
+		****************************
+		****************************
+		****************************
+		*/
+		/*
+		Update the bat, the ball and the HUD
+		*****************************
+		*****************************
+		*****************************
+		*/
+
+
+		/*
+		Draw the bat, the ball and the HUD
+		*****************************
+		*****************************
+		*****************************
+		*/
+
+	}
+	return 0;
 }
